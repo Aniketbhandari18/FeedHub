0120ab7c4ffd64746cc927a9f23639d0336b565b
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-06-18 23:01:11 +0530

    add: update gradient colors and add extra button size variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,12 +20,13 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        gradient: "bg-gradient-to-r from-blue-500 to-purple-400 text-white",
+        gradient: "bg-gradient-to-r from-blue-600 to-purple-600 text-white",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        xl: "h-12 rounded-xl px-8 has-[>svg]:px-6",
         icon: "size-9",
       },
       color: {
