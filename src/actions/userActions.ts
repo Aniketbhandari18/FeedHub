8c67f82a834c72bb2287045d5fdb61d914ab7d26
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-06-23 21:25:19 +0530

    feat: add clerkClient and update user metadata on user creation

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -1,9 +1,16 @@
+import { clerkClient } from "@/lib/clerkClient";
 import prisma from "@/lib/prisma";
 import { clerkUserPayload } from "@/types/user";
 import { User } from "@prisma/client";
 
 export const createUser = async (user: clerkUserPayload) => {
   const newUser: User = await prisma.user.create({ data: user });
+
+  await clerkClient.users.updateUserMetadata(user.clerkId, {
+    publicMetadata: {
+      dbUserId: newUser.id
+    }
+  })
 
   return newUser;
 };
